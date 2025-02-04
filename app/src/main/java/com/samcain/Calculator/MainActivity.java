1bc566eadfdf32d008eab51999fd5019ef48f2dc
Sam Cain <samcain7@gmail.com>
2025-02-04 13:42:24 -0600

    Setting array for button text Generating constraints/guidelines

--- app/src/main/java/com/samcain/Calculator/MainActivity.java
+++ app/src/main/java/com/samcain/Calculator/MainActivity.java
@@ -3,17 +3,15 @@ package com.samcain.Calculator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
-
-
 import com.samcain.Calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    private final int CHAIN_LENGTH = 9;
+    private final int BUTTON_COUNT = 20;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,37 +19,42 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        ConstraintLayout mainLayout = binding.main;
-        int[] viewIds = new int[CHAIN_LENGTH];
-
-        for (int i = 0; i < CHAIN_LENGTH; i ++) {
-            int id = View.generateViewId();
-            TextView textView = new TextView(this);
-            textView.setId(id);
-            textView.setTag("textView" + i);
-            textView.setText("TextView Chain Element " + i);
-            textView.setTextSize(24);
-            mainLayout.addView(textView);
-            viewIds[i] = id;
-        }
-
-        ConstraintSet set = new ConstraintSet();
-        set.clone(mainLayout);
-
-        for (int id: viewIds) {
-            set.connect(id, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
-            set.connect(id, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
-
-            set.createVerticalChain(ConstraintSet.PARENT_ID, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, viewIds, null, ConstraintSet.CHAIN_PACKED);
-
-            set.applyTo(mainLayout);
-        }
 
         initLayout();
     }
 
-
+    // Initialize the layout
     private void initLayout() {
+        ConstraintLayout constraintLayout = binding.main;
+        int id = View.generateViewId();
+
+        // Create output TextView
+        TextView output = new TextView(this);
+        output.setText("0");
+        output.setId(id);
+        output.setTextSize(24);
+        constraintLayout.addView(output);
+
+        // Create buttons
+        int[] buttonIds = new int[BUTTON_COUNT];
+        String[] buttonText = getResources().getStringArray(R.array.buttonNames);
+        for (int i = 0; i < BUTTON_COUNT; i++) {
+            TextView button = new TextView(this);
+            button.setText(buttonText[i]);
+            button.setId(buttonIds[i]);
+            button.setTextSize(16);
+            constraintLayout.addView(button);
+        }
+        // Connect buttons
+        for (int i = 0; i < BUTTON_COUNT; i++) {
+            ConstraintSet constraintSet = new ConstraintSet();
+            constraintSet.clone(constraintLayout);
+            constraintSet.connect(buttonIds[i], ConstraintSet.TOP, output.getId(), ConstraintSet.BOTTOM);
+        }
+
+
+
+
 
     }
 }
